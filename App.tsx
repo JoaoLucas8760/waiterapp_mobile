3cc4a9102ed3b253e2bc6f6bcd93c24106ce2f5d
perfluka8760 <canalluka8760@gmail.com>
2023-04-16 01:41:59 -0300

    flatlist

--- App.tsx
+++ App.tsx
@@ -1,42 +1,24 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { Text } from "./src/components/Text";
+import { Main } from "./src/Main";
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
     "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
-    " GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
     "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
   });
 
   if (!isFontsLoaded) {
-    return (
-      <View style={styles.notLoaded}>
-        <Text>Loading...</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+    return null;
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  notLoaded: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
