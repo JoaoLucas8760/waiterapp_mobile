1e8bacb7052b8aeec96d9196af64d75b0726fb47
perfluka8760 <canalluka8760@gmail.com>
2023-04-17 20:21:27 -0300

    header

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -24,7 +24,7 @@ export function Main() {
   return (
     <>
       <Container>
-        <Header />
+        <Header selectedTable={selectedTable} />
 
         <CategoriesContainer>
           <Categories />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,41 @@
+import { TouchableOpacity } from "react-native";
 import { Text } from "../Text";
-import { Container } from "./styles";
+import { Container, Content, OrderHeader } from "./styles";
 
-export function Header() {
+interface props {
+  selectedTable: string;
+}
+
+export function Header({ selectedTable }: props) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>
-        Bem Vindo (a) ao
-      </Text>
-      <Text size={24} weight="700">
-        WAITER<Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>
+            Bem Vindo (a) ao
+          </Text>
+          <Text size={24} weight="700">
+            WAITER<Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+
+      {selectedTable && (
+        <>
+          <Content>
+            <OrderHeader>
+              <Text size={24} weight="600">
+                Pedido
+              </Text>
+              <TouchableOpacity>
+                <Text color="#d73035" weight="600" size={14}>
+                  Cancelar pedidoo
+                </Text>
+              </TouchableOpacity>
+            </OrderHeader>
+          </Content>
+        </>
+      )}
     </Container>
   );
 }
